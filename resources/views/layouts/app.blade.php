972bc953857c4f88d23707f44d7dea3257705b17
Gürkan Şen <gurkan.sen35@gmail.com>
2018-04-27 02:12:28 +0300

    Navbar %70 tamamlandı

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
+    <script src="{{ asset('js/jquery-3.3.1.min.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/fontawesome-all.min.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,22 +1,66 @@
 <nav class="navbar">
-    <div class="navbar-inner container">
-        <a class="navbar-logo" href="{{ url('/') }}">
-            Proje Avcısı
-        </a>
+    <div class="navbar-inner">
+        <div class="navbar-left flex space-between vertical-center">
+            <a class="nav-logo" href="{{ url('/') }}">
+                Proje Avcısı
+            </a>
 
-        <ul class="navbar-nav flex space-between">
-            <li><a href="{{ route('share-project') }}" class="button blue-bg">+ Proje Paylaş</a></li>
-            @guest
-                <li><a class="nav-link" href="{{ route('login') }}">Giriş Yap</a></li>
-                <li><a class="nav-link" href="{{ route('register') }}">Üye Ol</a></li>
-            @else
-                <li>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                        <button type="button" name="button">Çıkış Yap</button>
-                    </form>
-                </li>
-            @endguest
-        </ul>
+            <div class="nav-search">
+                <form class="nav-search-form flex space-between vertical-center" action="" method="GET">
+                    @csrf
+                    <input type="text" name="q" class="nav-search-text" placeholder="Proje ara..">
+                    <button type="button" name="button" class="nav-search-btn"><i class="fas fa-search fa-lg"></i></button>
+                </form>
+            </div>
+        </div>
+
+        <div class="nav-right flex space-between vertical-center">
+            <div class="nav-categories-dropdown">
+                <a href="{{ url('kategoriler') }}" class="nav-categories-btn">
+                    <i class="fas fa-bars fa-lg"></i>&nbsp Kategoriler
+                </a>
+
+                <div class="sub-navbar">
+                    <ul class="sub-nav-categories">
+                        <li class="sm-category-card"><a href="#">Yazılım</a></li>
+                        <li class="sm-category-card"><a href="#">Tasarım</a></li>
+                        <li class="sm-category-card"><a href="#">Kanal</a></li>
+                        <li class="sm-category-card"><a href="#">Eğitim</a></li>
+                        <li class="sm-category-card"><a href="#">Araç</a></li>
+                        <li class="sm-category-card"><a href="#">Kitap</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <ul class="navbar-nav flex space-between vertical-center">
+                <li><a href="{{ route('share-project') }}" class="button blue-bg">+ Proje Paylaş</a></li>
+                @guest
+                    <li><a class="nav-link" href="{{ route('login') }}">Giriş Yap</a></li>
+                    <li><a class="nav-link" href="{{ route('register') }}">Üye Ol</a></li>
+                @else
+                    <li>
+                        <a href="{{ url('bildirimler') }}" class="nav-link">
+                            <i class="far fa-bell fa-lg"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="{{ url('uye/gurkansen') }}" class="nav-link">
+                            <i class="far fa-user fa-lg"></i>
+                        </a>
+                    </li>
+                    <!--<li>
+                        <a href="#" class="nav-link nav-image-link flex vertical-center">
+                            <img src="img/default.png" class="circle-image size-36" alt="{{ 'User Name' }}">
+                        </a>
+                    </li>-->
+                    <!--<li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button type="button" name="button">Çıkış Yap</button>
+                        </form>
+                    </li>-->
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
